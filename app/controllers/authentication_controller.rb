4aa4faea01c8b6568b8ccc2dead5b5211f11f4e8
Your Name <you@example.com>
2017-11-27 22:01:35 +0000

    User regist

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -2,11 +2,14 @@ class AuthenticationController < ApplicationController
 
   def signin
     
+    @success = false
+    @token = nil
+
     if params.has_key?(:username) && params.has_key?(:password) && params.has_key?(:name)
       
       matched = /[^'"\[\]{}]{6,15}/.match(params[:password])
-      
-      if match.eql?(params[:password])
+
+      if matched.to_s.eql?(params[:password])
         
         user = Player.new(
           name: params[:name],
@@ -14,11 +17,22 @@ class AuthenticationController < ApplicationController
           password: Digest::SHA256.hexdigest(params[:password])
           )
         user.save
-
         
+        filename = File.join(Rails.root, 'keys', 'private_key.pem')
+        rsa_private = OpenSSL::PKey::RSA.new(File.read(filename))
+        
+        payload = {
+          :user_id => user["id"],
+          :ip_address => request.remote_ip,
+          :expiration_date => DateTime.now + 3.hours
+        }
+    
+        @token = JWT.encode payload, rsa_private, 'RS256'
+        @success = true
 
       end
     end
+    render "login"
   end
 
   def login
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   post '/login', to: 'authentication#login'
+  post '/signin', to: 'authentication#signin'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
