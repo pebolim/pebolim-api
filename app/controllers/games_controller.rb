8c17f16af6d0ee2a4baf02e27d2af43710fec6ec
Your Name <you@example.com>
2018-01-06 22:29:17 +0000

    adapting

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -9,77 +9,47 @@ class GamesController < ApplicationController
     # end
 
     def gameDetails
-        @game = Game.find_by(:url => params[:id])       
-        @participations = @game.participations    
-        @status=200  
+        if @game = Game.find_by(:url => params[:id])  
+            @participations = @game.participations    
+            @status=200  
+        else
+            render json: {message:"Game not found", status:500}.to_json
+        end
     end
 
     #TODO: verificar parametros e validar o user
     def startGame     
         game = Game.find_by(:url => params[:id])   
+        if game.users.count == 4
+            game.state = 2
+            game.start_date = DateTime.now
+            game.is_locked = true
 
-        game.state = 2
-        game.start_date = DateTime.now
-        game.is_locked = true
-
-        #valida as equipas
-        game.teams.each do |team|
-            
-            users_teams =[];
-            team.users.each do |user|
-                users_teams.push(user.teams)
+            if game.save
+                render json: { message: "OK", status: 200 }.to_json   
+            else
+                render json: { message: "Is not possible to start the game, try again later", status: 500 }.to_json
             end
-
-            repeted_teams = users_teams.find_all { |e| users_teams.count(e) > 1 }
-            if(repeted_teams.length>1)
-                repeted_teams.drop(1).each do |repeted|
-                    Participation.where(:game=>game, :team=>repeted).delete
-                    Partnership.where(:team=>repeted).delete
-                    Team.find(repeted["id"]).delete
-                end
-                Participation.create(:game=>game, :team=>repeted.first)
-            end
-        end
-
-        if game.save
-            render json: { message: "OK", status: 200 }.to_json   
         else
-            render json: { message: "Not OK", status: 500 }.to_json
+            render json: { message: "Unsufficient players to start the game", status: 500 }.to_json 
         end
-    end
-
-    def getPlayers
-        game = Game.find_by(:url => params[:id])
-        teams = game.teams
-
-        result = [];
-        teams.each do |team| 
-            result.push({
-                "id":team["id"],
-                "name":team["name"],
-                "players":team.users.select(:id, :nickname)
-            })
-        end 
-
-        render json: { message: "OK", status: 200, teams: result }.to_json  
     end
 
     def joinGame
         get_user_by_token(request)
         if @status!=500 && @current_user!=0
             @game = Game.find_by(:url => params[:id])
-            user=User.find(@current_user)
 
             #remover a posição antiga
             @game.teams.each do |team|
-                if team.partnerships.exists?(:user=>user)
-                    team.partnerships.where(:user=>user).first.delete
+                if team.users.exists?(@current_user)
+                    team.partnerships.where(user_id:@current_user).first.delete
                 end
             end
             
             #inserir na nova posição
-            if Partnership.where(:team_id=>params[:teamid]).length < 2
-                new_partnership = Partnership.new(user_id:user["id"], team_id:params[:teamid])
+            if Partnership.where(team_id:params[:teamid]).length < 2
+                new_partnership = Partnership.new(user_id:@current_user, team_id:params[:teamid])
                 if new_partnership.save
                     render json: {message:"OK", status:200}.to_json
                 else
@@ -91,34 +61,6 @@ class GamesController < ApplicationController
         else
             render json: {message:"Unauthorized", status:500}.to_json
         end
-        
-        # @game.teams.where(:defender => params[:user_id]).update_all(defender: 0)
-
-        # @game.teams.update_all(:name => "x")
-        # flag = 0;
-        # @game.teams.each do |t|
-        #     if t.attacker == Integer(user_id)
-        #         t.attacker = 0;
-        #         flag = 1;
-        #     end
-        #     if t.defender == Integer(user_id)
-        #         t.defender = 0;
-        #         flag = 1;
-        #     end
-
-        # end
-
-        # if flag == 1
-        #     c.data["balance"] = "100"
-        #     c.data_will_change!
-        #     c.save
-        # end 
-        
-        #if @game.save
-            #render json: { message: "OK", status: 200 }.to_json   
-        # else
-        #     render json: { message: "Not OK", status: 500 }.to_json
-        # end
     end
 
     #post
@@ -126,27 +68,36 @@ class GamesController < ApplicationController
         get_user_by_token(request)
         if @status!=500 && @current_user!=0
             @params = game_params
-            date = Date.parse(@params[:start_date])
-            @game = Game.new(
-                local: @params[:local],
-                is_private: @params[:is_private],
-                url: SecureRandom.urlsafe_base64,
-                match_day: DateTime.new(date.year, date.month, date.mday, @params[:hour], @params[:minutes]),
-                owner: User.find(@current_user),
-                is_locked: false
-            )
-            if @game.save
-                team_1 = Team.new
-                team_2 = Team.new
-                if team_1.save && team_2.save
-                    participation_1=Participation.create(game:@game, team:team_1)
-                    participation_2=Participation.create(game:@game, team:team_2)
-                    render json: { message: "OK", status: 201, url_id: @game.url }.to_json 
+            byebug
+            if @params.has_key?(:local) && @params.has_key?(:hour) && @params.has_key?(:minutes) && @params.has_key?(:start_date)
+                date = Date.parse(@params[:start_date])
+                @game = Game.new(
+                    local: @params[:local],
+                    url: SecureRandom.urlsafe_base64,
+                    match_day: DateTime.new(date.year, date.month, date.mday, @params[:hour], @params[:minutes]),
+                    owner: User.find(@current_user)
+                )
+                if @params.has_key?(:is_private) && @params[:is_private]!=""
+                    @game.is_private= @params[:is_private];
+                end
+                if @params.has_key?(:to_teams) && @params[:to_teams]!=""
+                    @game.to_teams= @params[:to_teams];
+                end
+                if @game.save
+                    team_1 = Team.new
+                    team_2 = Team.new
+                    if team_1.save && team_2.save
+                        participation_1=Participation.create(game:@game, team:team_1)
+                        participation_2=Participation.create(game:@game, team:team_2)
+                        render json: { message: "OK", status: 201, url_id: @game.url }.to_json 
+                    else
+                        render json: { message: "The game cannot be initialized", status: 500 }.to_json
+                    end  
                 else
-                    render json: { message: "The game cannot be initialized", status: 500 }.to_json
-                end  
+                    render json: { message: "Cannot create a new game", status: 500 }.to_json
+                end
             else
-                render json: { message: "Cannot create a new game", status: 500 }.to_json
+                render json: {message:"You need to fill all fields", status:500}.to_json
             end
         else
             render json: {message:"Unauthorized", status:500}.to_json
@@ -165,7 +116,7 @@ class GamesController < ApplicationController
     end
 
     def game_params
-        params.require(:game).permit(:local,:is_private,:hour,:minutes,:start_date)
+        params.require(:game).permit(:local,:is_private,:hour,:minutes,:start_date,:to_teams)
     end
 
 end
--- app/views/games/gameDetails.json.jbuilder
+++ app/views/games/gameDetails.json.jbuilder
@@ -6,17 +6,28 @@ json.set! :game do
     json.url @game.url
     json.is_private @game.is_private
     json.local @game.local
-    json.owner @game.owner.id
+    json.owner_id @game.owner.id
+    json.owner_name @game.owner.nickname
     json.state @game.state
     json.start_date @game.start_date
     json.finish_date @game.finish_date
+    json.match_day @game.match_day
+    json.to_teams @game.to_teams
+end
 
-    json.set! :teams do
-        json.array! @participations.each do |participation|
-            json.id participation.team.id
-            json.name participation.team.name
-            json.is_winner participation.is_winner
-            json.goals participation.goals
+json.set! :teams do
+    json.array! @participations.each do |participation|
+        json.id participation.team.id
+        json.name participation.team.name
+        json.is_winner participation.is_winner
+        json.goals participation.goals
+
+        json.set! :players do
+            json.array! participation.team.users.each do |user|
+                json.id user.id
+                json.nickname user.nickname
+                json.image_url user.image_url
+            end
         end
     end
 end
